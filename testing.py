515ad1f27484780623dc6f8d50d944d59204fff3
William Zhang <willzhang05@gmail.com>
2020-03-28 15:55:05 -0400

    initially working?

--- testing.py
+++ testing.py
@@ -1,4 +1,11 @@
+import sys
 import populartimes
+from pprint import pprint
 
 
+API_KEY = sys.argv[1]
 
+data = populartimes.get_id(API_KEY, "ChIJSYuuSx9awokRyrrOFTGg0GY")
+pprint(data)
+data = populartimes.get(API_KEY, ["grocery_or_supermarket"], (48.132986, 11.566126), (48.142199, 11.580047))
+pprint(data)
